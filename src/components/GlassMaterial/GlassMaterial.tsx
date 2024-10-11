a3196bf9aaeeccf3b755c7993c4cc7554d2c9332
ChristofferBillman <christoffer.billman@gmail.com>
2024-10-11 23:14:40 +0200

    feat: GlassMaterial can now act as a button.

--- src/components/GlassMaterial/GlassMaterial.tsx
+++ src/components/GlassMaterial/GlassMaterial.tsx
@@ -1,22 +1,26 @@
-import { ReactNode } from 'react'
+import { createElement, forwardRef, ReactNode } from 'react'
 import style from './GlassMaterial.module.css'
 
 interface Props {
 	className?: string
 	children: ReactNode
 	onClick?: () => void
+	baseElement?: 'div' | 'button'
 }
 
-export function GlassMaterial({ className = '', children, onClick }: Props) {
+export const GlassMaterial = forwardRef(function ({ className = '', children, onClick, baseElement = 'div' }: Props, ref: React.ForwardedRef<HTMLDivElement>) {
 
 	const clickableStyle = onClick ? style.clickable : ''
 
-	return (
-		<div
-			className={`${style.glass} ${clickableStyle} ${className}`}
-			onClick={onClick}
-		>
-			{children}
-		</div>
-	)
-}
+	return createElement(
+        baseElement,
+        {
+            className: `${style.glass} ${clickableStyle} ${className}`,
+            onClick,
+            ref,
+            tabIndex: onClick ? 0 : undefined,
+            role: onClick ? 'button' : undefined
+        },
+        children
+    )
+})
