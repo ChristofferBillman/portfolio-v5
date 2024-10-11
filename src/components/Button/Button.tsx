2afddf3fe31e58b601a163ffe1d8462b062ceead
ChristofferBillman <christoffer.billman@gmail.com>
2024-10-11 23:13:51 +0200

    feat: button now has slots left and right of text.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,27 @@
+import { ReactNode } from 'react'
 import GlassMaterial from '../GlassMaterial'
 import style from './Button.module.css'
 
 interface Props {
 	text: string
 	onClick?: () => void
+	leftSlot?: ReactNode
+	rightSlot?: ReactNode
 }
-export function Button({ text, onClick }: Props) {
+export function Button({ text, onClick, leftSlot = '', rightSlot = '' }: Props) {
 	return (
 		<GlassMaterial
 			className={style.button}
 			onClick={onClick}
+			baseElement='button'
 		>
-			<a className={style.link}>{text}</a>
+			{leftSlot}
+			<a
+				className={style.link}
+			>
+				{text}
+			</a>
+			{rightSlot}
 		</GlassMaterial>
 	)
 }
