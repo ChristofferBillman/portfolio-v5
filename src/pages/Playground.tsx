f3b2e4df75f597daaa50b4996571ca659a3f0c1b
ChristofferBillman <christoffer.billman@gmail.com>
2024-10-11 23:14:50 +0200

    feat: cleanup

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 import Button from "../components/Button"
 import GlassMaterial from "../components/GlassMaterial"
+import Icon from "../components/Icon"
+import SliderSelector from "../components/SliderSelector"
 import useAnimatedBackground from "../contexts/AnimatedBackgroundContext"
 
 export default function Playground() {
 
 	const animationController = useAnimatedBackground()
-	if (!animationController) throw new Error("Used outside context,.")
+	const [selectedItem, setSelection] = useState('List')
 
 	return (
 		<>
@@ -15,30 +18,34 @@ export default function Playground() {
 				<p>This glass material is so cool! I can't believe that gradient borders + border radius + transparency is possible!!!</p>
 			</GlassMaterial>
 			<div style={{margin: '1rem'}}/>
-			<div style={{display: 'flex', gap: '1rem'}}>
+			<div style={{display: 'flex', gap: '1rem', flexWrap: 'wrap'}}>
 			<Button
 				onClick={() => {
 					animationController.setColors(['#C91010', '#CE522D'])
 				}}
-				text='Make me red!'
+				text='Red'
+				rightSlot={<Icon name='palette' size='0.75rem'/>}
 			/>
 			<Button
 				onClick={() => {
 					animationController.setColors(['#18C754', '#096423', '#99B916'])
 				}}
-				text='Make me green!'
+				text='Green'
+				rightSlot={<Icon name='palette' size='0.75rem'/>}
 			/>
 			<Button
 				onClick={() => {
 					animationController.setColors(['#161616', '#353535', '#0A0A0A'])
 				}}
-				text='Make me black!'
+				text='Black'
+				rightSlot={<Icon name='palette' size='0.75rem'/>}
 			/>
 			<Button
 				onClick={() => {
 					animationController.setColors(['#2BA1FC','#84B2C0','#0D5473','#1980FF','#10ADC9'])
 				}}
-				text='Make me blue!'
+				text='Blue'
+				rightSlot={<Icon name='palette' size='0.75rem'/>}
 			/>
 
 			<Button
@@ -46,6 +53,13 @@ export default function Playground() {
 					animationController.restoreDefaultColors()
 				}}
 				text='Restore defaults'
+				rightSlot={<Icon name='settings_backup_restore' size='0.75rem'/>}
+			/>
+
+			<SliderSelector
+				selectedValue={selectedItem}
+				setSelection={setSelection}
+				items={[{value: 'List', icon: 'list'},{value: 'Grid', icon: 'grid_view'},{value: 'Bento', icon: 'bento'}]}
 			/>
 			</div>
 		</>
