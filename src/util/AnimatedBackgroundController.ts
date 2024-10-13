75f1173fa7dc54c79a453093bbeb99e63a516d78
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-13 12:38:24 +0200

    feat: minor changees to animated background to improve performance.

--- src/util/AnimatedBackgroundController.ts
+++ src/util/AnimatedBackgroundController.ts
@@ -86,7 +86,6 @@ export default class AnimatedBackgroundController {
 <svg xmlns="http://www.w3.org/2000/svg">
 	<defs>
 		<filter id="goo">
-			<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
 			<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
 			<feBlend in="SourceGraphic" in2="goo" />
 		</filter>
