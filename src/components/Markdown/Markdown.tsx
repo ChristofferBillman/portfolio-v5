d0cd7fbbef77cbb691ddceeec7422814076f5c4a
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-15 00:13:26 +0200

    feat: added Markdown component.

---
+++ src/components/Markdown/Markdown.tsx
@@ -0,0 +1,60 @@
+import { createElement, ReactNode } from "react"
+
+interface Props {
+	content: string
+}
+
+export function Markdown({content}: Props) {
+	return parse(content)
+}
+
+// This is by no means a real parser, but a quick and easy solution to handle the sunnyday scenario.
+function parse(content: string): ReactNode {
+	const elements: ReactNode[] = []
+
+	const lines = content.split(/\r?\n|\r|\n/g)
+
+	lines.forEach(line => {
+		const trimmed = line.trim()
+		if(line.length == 0) return whitespace()
+		if(trimmed[0] == '#') return h(trimmed)
+		if(trimmed[0] == '!') return img(trimmed)
+		return p(trimmed)
+	})
+	return elements
+
+	function h(line: string) {
+		// Find first occurence of space. The index of that space is the header level.
+		let headerLevel = line.indexOf(' ')
+		if(headerLevel > 6) headerLevel = 6
+
+		const content = line.substring(headerLevel)
+
+		elements.push(createElement(`h${headerLevel}`, undefined, content))
+	}
+
+	function p(line: string) {
+		elements.push(createElement('p', undefined, line))
+	}
+
+	function whitespace() {
+		elements.push(<br/>)
+	}
+
+	function img(line: string) {
+		// ![Alttext](./public/img/chare/Hero.png "This is the caption")
+		expectEqual(line[1], '[')
+		const altEnd = line.indexOf(']', 1)
+		const alt = line.substring(2, altEnd)
+		const srcEnd = line.indexOf('"', altEnd) - 1
+		const src = line.substring(altEnd + 2, srcEnd)
+		const caption = line.substring(srcEnd + 2, line.indexOf('"',srcEnd + 2))
+
+		elements.push(createElement('img', { src, alt }))
+		elements.push(createElement('span', {className: 'caption'} , caption))
+	}
+
+	function expectEqual(char: string, char2: string) {
+		if(char != char2) throw new Error('Markdown parse error.')
+	}
+}
