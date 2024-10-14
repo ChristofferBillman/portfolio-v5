179b23db57a7cca691ab33227e5fae814a695286
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-14 19:17:56 +0200

    feat: implemented Modal component.

---
+++ src/components/common/Modal/Modal.tsx
@@ -0,0 +1,52 @@
+import { Dispatch, RefObject, SetStateAction, useCallback, useLayoutEffect, useRef } from 'react'
+
+import TransitionLifecycle from '../../TransitionLifecycle'
+
+import useOutsideClick from '../../../hooks/useOutsideClick'
+import GlassMaterial from '../GlassMaterial'
+
+import style from './Modal.module.css'
+
+interface Props {
+	visible: boolean
+    setVisible: Dispatch<SetStateAction<boolean>>
+    children: JSX.Element | JSX.Element[]
+	className?: string
+	bgRef: RefObject<HTMLDivElement>
+}
+
+export function Modal({ visible, setVisible, children, className = '', bgRef}: Props) {
+
+	const ref = useRef<HTMLDivElement>(null)
+	useOutsideClick(() => setVisible(false), [ref])
+
+	const scaleBody = useCallback((scale: number) => {
+		if(bgRef.current) {
+			bgRef.current.style.transitionDuration = '500ms'
+			bgRef.current.style.transform = `scale(${scale})`
+		}
+	}, [bgRef])
+
+	useLayoutEffect(() => {
+		if(visible) scaleBody(0.9)
+		else scaleBody(1)
+	},[visible, scaleBody])
+
+	return (
+		<TransitionLifecycle
+			willRender={visible}
+			transition={{
+				initial: { opacity: 0, transform: 'translate(-50%, -50%) scale(1.2)', transformOrigin: 'center' },
+				transition: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)', transformOrigin: 'center' },
+				exit: { opacity: 0, transform: 'translate(-50%, -50%) scale(1.2)', transformOrigin: 'center' },
+				duration: 500
+			}}
+			style={{zIndex: 1000, position: 'fixed', top:'50%', left:'50%'}}
+			ref={ref}
+		>
+			<GlassMaterial className={`${style.modal} ${className}`}>
+				{children}
+			</GlassMaterial>
+		</TransitionLifecycle>
+	)
+}
--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,67 +1,97 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Button from "../components/common/Button"
 import GlassMaterial from "../components/common/GlassMaterial"
 import Icon from "../components/common/Icon"
 import SliderSelector from "../components/common/SliderSelector"
 import useAnimatedBackground from "../contexts/AnimatedBackgroundContext"
+import { Modal } from "../components/common/Modal/Modal"
 
 export default function Playground() {
 
 	const animationController = useAnimatedBackground()
 	const [selectedItem, setSelection] = useState('List')
 
+	const [modalVisible, setModalVisible] = useState(false)
+	const contentRef = useRef<HTMLDivElement>(null)
+
 	return (
 		<>
-			<h1>Playground</h1>
-			<p>This is where I test different components and features.</p>
-			<div style={{margin: '1rem'}}/>
-			<div style={{display: 'flex', gap: '1rem', flexWrap: 'wrap', marginBottom: '1rem'}}>
-			<Button
-				onClick={() => {
-					animationController.setColors(['#C91010', '#CE522D'])
-				}}
-				text='Red'
-				rightSlot={<Icon name='palette' size='0.75rem'/>}
-			/>
-			<Button
-				onClick={() => {
-					animationController.setColors(['#18C754', '#096423', '#99B916'])
-				}}
-				text='Green'
-				rightSlot={<Icon name='palette' size='0.75rem'/>}
-			/>
-			<Button
-				onClick={() => {
-					animationController.setColors(['#161616', '#353535', '#0A0A0A'])
-				}}
-				text='Black'
-				rightSlot={<Icon name='palette' size='0.75rem'/>}
-			/>
-			<Button
-				onClick={() => {
-					animationController.setColors(['#2BA1FC','#84B2C0','#0D5473','#1980FF','#10ADC9'])
-				}}
-				text='Blue'
-				rightSlot={<Icon name='palette' size='0.75rem'/>}
-			/>
+			<div ref={contentRef}>
+				<h1>Playground</h1>
+				<p>This is where I test different components and features.</p>
+				<div style={{ margin: '1rem' }} />
+				<div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap', marginBottom: '1rem' }}>
+					<Button
+						onClick={() => {
+							animationController.setColors(['#C91010', '#CE522D'])
+						}}
+						text='Red'
+						rightSlot={<Icon name='palette' size='0.75rem' />}
+					/>
+					<Button
+						onClick={() => {
+							animationController.setColors(['#18C754', '#096423', '#99B916'])
+						}}
+						text='Green'
+						rightSlot={<Icon name='palette' size='0.75rem' />}
+					/>
+					<Button
+						onClick={() => {
+							animationController.setColors(['#161616', '#353535', '#0A0A0A'])
+						}}
+						text='Black'
+						rightSlot={<Icon name='palette' size='0.75rem' />}
+					/>
+					<Button
+						onClick={() => {
+							animationController.setColors(['#2BA1FC', '#84B2C0', '#0D5473', '#1980FF', '#10ADC9'])
+						}}
+						text='Blue'
+						rightSlot={<Icon name='palette' size='0.75rem' />}
+					/>
 
-			<Button
-				onClick={() => {
-					animationController.restoreDefaultColors()
-				}}
-				text='Restore defaults'
-				rightSlot={<Icon name='settings_backup_restore' size='0.75rem'/>}
-			/>
+					<Button
+						onClick={() => {
+							animationController.restoreDefaultColors()
+						}}
+						text='Restore defaults'
+						rightSlot={<Icon name='settings_backup_restore' size='0.75rem' />}
+					/>
 
-			<SliderSelector
-				selectedValue={selectedItem}
-				setSelection={setSelection}
-				items={[{text: 'List', value: 'list', icon: 'list'},{text: 'Grid', value: 'grid', icon: 'grid_view'},{text: 'Bento', value: 'bento', icon: 'bento'}]}
-			/>
+					<Button
+						onClick={() => {
+							setModalVisible(true)
+						}}
+						text='Open modal'
+						rightSlot={<Icon name='expand_content' size='0.75rem' />}
+					/>
+
+					<SliderSelector
+						selectedValue={selectedItem}
+						setSelection={setSelection}
+						items={[{ text: 'List', value: 'list', icon: 'list' }, { text: 'Grid', value: 'grid', icon: 'grid_view' }, { text: 'Bento', value: 'bento', icon: 'bento' }]}
+					/>
+				</div>
+				<GlassMaterial className='g-test'>
+					<p>Glass Material</p>
+				</GlassMaterial>
 			</div>
-			<GlassMaterial className='g-test'>
-				<p>Glass Material</p>
-			</GlassMaterial>
+
+			<Modal
+				visible={modalVisible}
+				setVisible={setModalVisible}
+				bgRef={contentRef}
+				className="modal-test"
+			>
+				<p>This is some modal content. I should have the effect present in visionOS modals.</p>
+				<Button
+					onClick={() => {
+						setModalVisible(false)
+					}}
+					text='Close'
+					rightSlot={<Icon name='close' size='0.75rem' />}
+				/>
+			</Modal>
 		</>
 	)
 }
