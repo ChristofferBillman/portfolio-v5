f60313c487fe832d16d4f55e29a7b1d87831f329
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-22 16:23:06 +0200

    wip: footer contact and links added.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "../../contexts/TranslationContext";
+import A from "../common/A";
 import GlassMaterial from "../common/GlassMaterial";
 import style from './Footer.module.css'
 
@@ -8,10 +9,22 @@ export function Footer() {
 
 	return (
 	<GlassMaterial className={style.footer}>
-		<div>
+		<div className={style.infoContainer}>
 			<h2>Christoffer<br/>Billman</h2>
 			<p>(c) Christoffer Billman 2024.<br/>{translation.FooterTagline}</p>
 		</div>
-		<p>{translation.BuiltWithReact} <a href='https://github.com/ChristofferBillman/portfolio/'>{translation.SourceCode}</a><br/>Version 0.0.0</p>
+		<div className={`${style.infoContainer} ${style.alignRight}`}>
+			<h3>{translation.Contact}</h3>
+			<p>+46 07 577 22 97</p>
+			<p>christoffer.billman@gmail.com</p>
+		</div>
+		<div className={style.infoContainer}>
+			<p>{translation.BuiltWithReact} <a href='https://github.com/ChristofferBillman/portfolio/'>{translation.SourceCode}</a><br/>Version 0.0.0</p>
+		</div>
+		<div className={`${style.infoContainer} ${style.alignRight}`}>
+			<h3>{translation.LinksAndResources}</h3>
+			<A href="https://www.linkedin.com/in/christoffer-billman-840029212">LinkedIn</A>
+			<A href="https://github.com/ChristofferBillman">GitHub</A>
+		</div>
 	</GlassMaterial>)
 }
