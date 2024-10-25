e6bf7b77ed308f665a59f77dd2807acd5c129b10
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-25 22:26:14 +0200

    feat: changing this for debug purposes on iPad.

--- src/util/getProjects.ts
+++ src/util/getProjects.ts
@@ -1,10 +1,9 @@
 import { projectsSE } from '../data/projectsSE'
-import {projectsEN } from '../data/projectsEN'
 import getLocale from "./getLocale"
 
 export default function getProjects() {
 	switch(getLocale()) {
-		case 'EN': return projectsEN.sort((a,b) => a.position - b.position)
+		case 'EN': return projectsSE.sort((a,b) => a.position - b.position)
 		case 'SE': return projectsSE.sort((a,b) => a.position - b.position)
 	}
 }
