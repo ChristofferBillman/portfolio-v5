328da1e380a0ee978b9a9e547108619e97c588af
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-27 09:51:47 +0100

    refactor: fixed A so that icon is optional and centered too.

--- src/components/common/A/A.tsx
+++ src/components/common/A/A.tsx
@@ -1,19 +1,21 @@
-import { ReactNode } from "react"
 import Icon from "../Icon"
+import style from './A.module.css'
 
 interface Props {
 	href: string
-	children: ReactNode
+	iconName?: string
+	text: string
 }
 
-export function A({href, children}: Props) {
+export function A({href, iconName, text}: Props) {
 	return (
 		<a
 			href={href}
 			target='_blank'
+			className={style.link}
 		>
-			{children}
-			<Icon name='open_in_new'/>
+			<span>{text}</span>
+			{iconName && <Icon name={iconName}/>}
 		</a>
 	)
 }
