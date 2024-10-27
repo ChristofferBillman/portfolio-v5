9f6b96879380dd8734b3754b216168761d78698b
Christoffer Billman <christoffer.billman@gmail.comm>
2024-10-27 09:52:35 +0100

    feat: Button now has a white variant, also styled its outline when navigating with keyboard.

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,25 +3,42 @@ import GlassMaterial from '../GlassMaterial'
 import style from './Button.module.css'
 
 interface Props {
-	text: string
+	text?: string
 	onClick?: () => void
 	leftSlot?: ReactNode
 	rightSlot?: ReactNode
+	type?: 'white' | 'glass'
 }
-export function Button({ text, onClick, leftSlot = '', rightSlot = '' }: Props) {
+export function Button({ text, onClick, leftSlot = '', rightSlot = '', type = 'glass' }: Props) {
+
+	const Wrapper = type == 'glass' ? GlassMaterial : WhiteMaterial
+
 	return (
-		<GlassMaterial
+		<Wrapper
 			className={style.button}
 			onClick={onClick}
 			baseElement='button'
 		>
 			{leftSlot}
-			<a
-				className={style.link}
-			>
-				{text}
-			</a>
+			{text &&
+				<a className={style.link}>
+					{text}
+				</a>
+			}
 			{rightSlot}
-		</GlassMaterial>
+		</Wrapper>
+	)
+}
+
+interface WhiteMaterialProps {
+	children: ReactNode
+	onClick?: () => void
+	className: string
+}
+function WhiteMaterial({children, onClick, className}: WhiteMaterialProps) {
+	return (
+		<button className={`${className} ${style.whiteWrapper}`} onClick={onClick}>
+			{children}
+		</button>
 	)
 }
--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -2,12 +2,13 @@ interface Props {
 	name: string
 	size?: string
 	color?: string
+	className?: string
 }
 
-export function Icon({name, color = 'rgba(255,255,255,0.8)'}: Props) {
+export function Icon({name, color = 'inherit', className = ''}: Props) {
 	return (
 		<span
-            className='material-symbols-rounded'
+            className={`${className} material-symbols-rounded`}
             style={{
 				color,
                 fontSize: 'inherit',
