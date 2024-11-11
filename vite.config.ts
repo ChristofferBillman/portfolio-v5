eb6cc8bf3cc07b57178bcf2d8f640ccade4fcd50
ChristofferBillman <christoffer.billman@gmail.com>
2024-11-11 14:48:57 +0100

    feat: configured automatic generation of sitemap and robots.txt.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+import { projectsSE } from './src/data/projectsSE'
 
 import {calculateDirSizesPlugin} from './bundleSizePlugin'
 
+
+const dynamicRoutes = ['/about', '/contact', '/projects', ...projectsSE.map(project => `/projects/${project.id}`)]
+
 export default defineConfig({
-  plugins: [react(), calculateDirSizesPlugin()],
-})
+  plugins: [
+	react(),
+	calculateDirSizesPlugin(),
+	Sitemap({
+		hostname: 'https://christofferbillman.se',
+		dynamicRoutes,
+		exclude: ['/404'],
+		changefreq: 'monthly'
+	}),
+  ],
+}) 
